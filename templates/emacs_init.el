076019ff4c35fbe1a9ad3dfb8e3ca99eaea8d45c
Holger Karl <hkarl@ieee.org>
2021-07-02 17:50:11 +0200

    and spell it correctly ...

---
+++ templates/emacs_init.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(current-language-environment "UTF-8")
+ '(org-latex-default-packages-alist
+   '(("AUTO" "inputenc" t
+      ("pdflatex"))
+     ("T1" "fontenc" t
+      ("pdflatex"))
+     ("" "graphicx" t nil)
+     ("" "grffile" t nil)
+     ("" "longtable" nil nil)
+     ("" "wrapfig" nil nil)
+     ("" "rotating" nil nil)
+     ("normalem" "ulem" t nil)
+     ("" "amsmath" t nil)
+     ("" "textcomp" t nil)
+     ("" "amssymb" t nil)
+     ("" "capt-of" nil nil)
+     ("" "xr-hyper" nil nil)
+     ("" "hyperref" nil nil)))
+ '(org-latex-hyperref-template nil)
+ '(org-latex-prefer-user-labels t)
+ )
